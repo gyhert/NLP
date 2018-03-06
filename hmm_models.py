ad2062ebce2d9481cb31263826c994f3eed0ef2d
pengw00 <water685@hotmail.com>
2018-03-06 11:03:09 -0600

    s

--- hmm_models.py
+++ hmm_models.py
@@ -1,12 +1,14 @@
 file = open('train.conll')
 content = file.read()
 f = content.split('\n\n')
+"""
 d = str(f[0])
 g = d.split('\n')
 j = str(g[0])
 h = j.split('\t')
 print(g)
 print(h)
+"""
 len1 = 0
 len2 = 0
 
@@ -14,6 +16,8 @@ len2 = 0
 
 #all words
 word = []
+#word_fre
+word_fre = {}
 #all tag
 tag = []
 #每个词出现的概率
@@ -36,22 +40,20 @@ for i in range(len(f)):
     g = d.split('\n')
     len1 +=1
     """word"""
+    """
     line_first = (g[0]).split('\t')
     if line_first[0] in pi:
         pi[line_first[0]] +=1
     else:
         pi[line_first[0]] = 1
+    """
     for j in range(len(g)):
+        len2 +=1
         item = g[j].split('\t')
         if item[0] not in word:
            word.append(item[0])
-           if item[0] in fre:
-               fre[item[0]] +=1
-           else:
-               fre[item[0]] = 1
         if item[-1] == '':
             print(item)
-
         if item[-1] not in tag:
             tag.append(item[-1])
 print(len(word))
@@ -64,6 +66,7 @@ print(tag)
 print(len(tag))
 print(len1)
 print(len(f))
+"""initial A B """
 for i in tag:
     pi[i] = 0
     fre[i] = 0
@@ -73,6 +76,43 @@ for i in tag:
         A[i][j] = 0
     for j in word:
         B[i][j] = 0
+L = 0
+for i in range(len(f)):
+    d = str(f[i])
+    g = d.split('\n')
+    line_first = g[0].split('\t')
+    if line_first[-1] in pi:
+        pi[line_first[-1]] += 1
+    for j in range(len(g)):
+        post = g[j].split('\t')
+        prev = g[j-1].split('\t')
+        if j > 0:
+           A[prev[-1]][post[-1]] += 1
+        if post[-1] in tag:
+           B[post[-1]][post[0]] += 1
+print(L)
+
+cx = {}
+cy = {}
+for i in tag:
+    cx[i] = 0
+    cy[i] = 0
+    pi[i] = pi[i] * 1.0 /len1
+    for j in tag:
+        if A[i][j] == 0:
+            cx[i] += 1
+            A[i][j] = 0.5
+    for j in word:
+        if B[i][j] == 0:
+           cy[i] += 1
+           B[i][j] = 0.5
+
+print(pi)
+
+
+
+
+
 
 
 """
