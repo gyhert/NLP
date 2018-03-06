23a3354c0fba3b37186b3855df123110e3d38e82
pengw00 <water685@hotmail.com>
2018-03-05 20:42:52 -0600

    d

--- hmm_models.py
+++ hmm_models.py
@@ -1,11 +1,34 @@
-file = open('dev.conll')
+file = open('train.conll')
 content = file.read()
 f = content.split('\n\n')
+d = str(f[0])
+g = d.split('\n')
+len1 = 0
+len2 = 0
+word = []
+tag = []
+for i in range(len(f)):
+    d = str(f[i])
+    g = d.split('\n')
+    len1 +=1
+    for item in g:
+        item = item.split('\t')
+        len2 +=1
+        if item[0] not in word:
+           word.append(item[0])
+        if item[-1] not in tag:
+            tag.append(item[-1])
+print(word)
+print(len(word))
+for i in range(len(tag)):
+    if tag[i] == '':
+        tag.remove('')
 
-f = str(f[0])
-print(f)
+print(tag)
+print(len(tag))
+print(len1)
+print(len2)
 print(len(f))
-
 
 
 """
